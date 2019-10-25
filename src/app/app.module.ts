fecd984d2d83187ba6087c1e5ed0752cb4a26d12
Art Schedrov <art.worknsk@gmail.com>
2019-10-26 03:33:50 +0700

    done. add material 26.10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,34 @@ import {HttpClientModule} from '@angular/common/http';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 import {WeatherService} from './weather.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './material/material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CurrentWeatherComponent
+    CurrentWeatherComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [WeatherService],
   bootstrap: [AppComponent]
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    MatButtonModule,
+    MatCardModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
